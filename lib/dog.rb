9d8a55559a3f40526447b01930e462f21eabd853
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-11 03:51:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,16 @@
 class Dog
 
-  def name=
-
+  def name=(dogs_name)
+    my_dogs_name = dogs_name
   end
-End
+
+  def name
+    my_dogs_name
+  end
+end
+
+fido = Dpg.new
+fido.name = "Fido"
+
+fido.bark
+puts "woof!"
